0620a5e357ac0d0852dddef6d74bc5894a8ce7ab
Oscar Carrasco <ocarrasco@grupogarflo.com>
2019-09-04 14:26:02 -0500

    Migrations
    Añadiendo migraciones

---
+++ database/migrations/2019_09_04_113125_create_request_info_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('request_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_system')->unsigned()->nullable();
+            $table->integer('id_payer')->unsigned()->nullable();
+            $table->string('id_reservation')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('reference')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_system')->references('id')->on('systems')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreign('id_payer')->references('id')->on('payer_data')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request_info');
+    }
+}
---
+++ database/migrations/2019_09_04_113330_create_transaction_response_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionResponseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_response', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_request_info')->nullable();
+            $table->string('id_order')->nullable();
+            $table->string('id_transaction')->nullable();
+            $table->string('status')->nullable();
+            $table->string('response_code')->nullable();
+            $table->string('pending_reason')->nullable();
+            $table->string('url_payment_recipt_html')->nullable();
+            $table->string('url_payment_recipt_pdf')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_request_info')->references('id')->on('request_info')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_response');
+    }
+}
